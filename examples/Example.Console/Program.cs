0709e79c11a272fbb449dac1edcc5371ee35b057
Benn Lazell <benn@redrabbits.co.uk>
2019-06-18 12:23:14 +0100

    Simple DirectoryLoader example

---
+++ examples/Example.Console/Program.cs
@@ -0,0 +1,55 @@
+﻿using Example.Extensibility;
+using Example.Extensibility.Events;
+using Example.Extensibility.Models;
+using System;
+using System.IO;
+
+namespace Example.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Modules");
+            var events = new ExampleEvents();
+            var host = new ExampleEventsHost(path, events);
+            string line;
+            int id = 1;
+            while ((line = Prompt()) != "exit")
+            {
+                Console.WriteLine($"Receiving new message '{line}' ... beep boop...");
+
+                var onReceivedEventArgs = new OnMessageReceivedEventArgs(line);
+                host.InvokeOnMessageReceived(onReceivedEventArgs);
+                
+                if(onReceivedEventArgs.Cancel)
+                {
+                    Console.WriteLine(" => Message Aborted!");
+                    continue;
+                }
+
+                // Module could augment the message
+                line = onReceivedEventArgs.Message;
+
+                Console.WriteLine($"Message received: {line}");
+
+                // Do something constructive
+                var message = new Message { MessageText = line, Id = id++ };
+
+                var onMessageCreatedEventArgs = new OnMessageCreatedEventArgs(message);
+                host.InvokeOnMessageCreated(onMessageCreatedEventArgs);
+
+                // All done, moving on...
+            }
+        }
+
+        static string MessagePrompt = "Enter a message: ";
+        static string Prompt()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(MessagePrompt);
+            Console.ResetColor();
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ examples/Example.Module2/ArchiveModule.cs
@@ -0,0 +1,14 @@
+﻿using Example.Extensibility.Events;
+using Extensible.Interfaces;
+using System;
+
+namespace Example.Module2
+{
+    public class ArchiveModule : IModule<ExampleEvents>
+    {
+        public void Initialize(ExampleEvents events)
+        {
+            events.OnMessageCreated += (e) => Console.WriteLine($" => [ArchiveModule] Message created: Id={e.Message.Id}, MessageText={e.Message.MessageText}");
+        }
+    }
+}
