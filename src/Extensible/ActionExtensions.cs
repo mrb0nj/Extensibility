e1e67f6544194b821ebc600ec7d65338593726fe
Benn Lazell <benn@redrabbits.co.uk>
2019-06-20 17:55:29 +0100

    Module Priority
    Modules can be decorated with [ModulePriority] attribute to provide what can only be described as a 'best guess priority system'.
    
    It's only best guess since the Modules can be dynamically loaded and there is no real way of knowing what priority other modules may have.

---
+++ src/Extensible/ActionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Extensible
+{
+    public static class ActionExtensions
+    {
+        public static Delegate[] GetInvocationListByPriority<T>(this Action<T> action)
+        {
+            return action.GetInvocationList()
+                .Select(method => new PrioritizedDelegate
+                    {
+                        Delegate = method,
+                        Priority = (
+                                method.Target
+                                    .GetType()
+                                    .GetCustomAttributes(true)
+                                    .Where(o => o is ModulePriorityAttribute)
+                                    .FirstOrDefault() as ModulePriorityAttribute
+                                )?.Priority ?? 1
+                    })
+                .OrderByDescending(pd => pd.Priority)
+                .Select(pd => pd.Delegate)
+                .ToArray();
+        }
+
+        class PrioritizedDelegate
+        {
+            public int Priority { get; set; }
+            public Delegate Delegate { get; set; }
+        }
+    }
+}
